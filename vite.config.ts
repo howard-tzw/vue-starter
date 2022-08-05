8e72c9766966b7b320d2dfff68b082a508064b22
Howard <howard.tzw@gmail.com>
2022-08-05 17:54:41 +0800

    add: github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,23 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vue-starter/',
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      '@': resolve(__dirname, './src'),
     },
   },
   server: {
     proxy: {
-      "/server": {
-        target: "https://staging.disfactory.tw",
+      '/server': {
+        target: 'https://staging.disfactory.tw',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/server/, ""),
+        rewrite: path => path.replace(/^\/server/, ''),
       },
     },
   },
-});
+})
