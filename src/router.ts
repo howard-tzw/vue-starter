816f75176484bb03c0364a6f0cfbd7307c8be2ac
Howard <howard.tzw@gmail.com>
2023-05-19 16:32:54 +0800

    dev: vue3-carousel

--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import Home from '@/views/Home.vue'
+import Carousel from '@/views/Carousel.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  // Fallback route for handling 404s
-  {
-    path: '/:pathMatch(.*)*',
-    name: '404',
-    component: () => import('./views/Error404.vue'),
-  },
+	{
+		path: '/',
+		name: 'Home',
+		component: Home,
+	},
+	{
+		path: '/carousel',
+		name: 'Carousel',
+		component: Carousel,
+	},
+	// Fallback route for handling 404s
+	{
+		path: '/:pathMatch(.*)*',
+		name: '404',
+		component: () => import('./views/Error404.vue'),
+	},
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+	history: createWebHistory(),
+	routes,
 })
 
 export default router
