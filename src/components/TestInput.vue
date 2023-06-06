00618071741578212bf2aaabe631ab66c5784b68
Howard <howard.tzw@gmail.com>
2023-06-06 19:52:31 +0800

    dev: defineModel

---
+++ src/components/TestInput.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const modelValue = defineModel()
+</script>
+
+<template>
+	<div>
+		<input class="input input-bordered" v-model="modelValue" />
+	</div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import TestInput from '@/components/TestInput.vue'
+
+const text = ref('')
+
+watch(text, () => {
+	console.log(text.value)
+})
+</script>
 
 <template>
-	<div>Hello World</div>
+	<div>
+		<TestInput v-model="text"></TestInput>
+	</div>
 </template>
 
 <style lang="css"></style>
