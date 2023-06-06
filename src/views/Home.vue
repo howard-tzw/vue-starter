c290d0207e9fd41cdd0e27b9b76ad22e61823f00
Howard <howard.tzw@gmail.com>
2023-06-06 18:57:18 +0800

    reset starter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,7 @@
-<template>
-	<div>
-		<div class="navbar bg-neutral text-neutral-content">
-			<a class="btn btn-ghost normal-case text-xl">Vue Starter</a>
-		</div>
+<script setup lang="ts"></script>
 
-		<div class="h-[calc(100vh+1000px)]">content</div>
-	</div>
+<template>
+	<div>Hello World</div>
 </template>
+
+<style lang="css"></style>
